17c39e618ad6c62febf52558aa7a04d79b64a06e
smsahariar1 <smsahariarkhan@gmail.com>
2023-01-01 00:26:15 +0600

    c

---
+++ for.js
@@ -0,0 +1,11 @@
+/*
+The 'for' loop is the most compact form of looping. It includes the following three important parts −
+
+The loop initialization where we initialize our counter to a starting value. The initialization statement is executed before the loop begins.
+
+The test statement which will test if a given condition is true or not. If the condition is true, then the code given inside the loop will be executed, otherwise the control will come out of the loop.
+
+The iteration statement where you can increase or decrease your counter.
+
+You can put all the three parts in a single line separated by semicolons.
+*/
---
+++ while.js
@@ -0,0 +1,3 @@
+/*
+The most basic loop in JavaScript is the while loop. The purpose of a while loop is to execute a statement or code block repeatedly as long as an expression is true. Once the expression becomes false, the loop terminates.
+*/
